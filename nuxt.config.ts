48c189c19694e0eeb05eb984a674b03782814278
RomaFedoro <fedorovroman1506@gmail.com>
2023-01-24 11:54:45 +0700

    hotfix(config): clear include scss variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,15 +20,6 @@ export default defineNuxtConfig({
       apiURL: 'http://92.125.33.134:3000/api',
     },
   },
-  vite: {
-    css: {
-      preprocessorOptions: {
-        scss: {
-          additionalData: '@use "@/assets/styles/_variables.scss" as *;',
-        },
-      },
-    },
-  },
   css: ['@/assets/styles/main.scss'],
   typescript: {
     typeCheck: true,
